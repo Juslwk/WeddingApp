1b65cbc42df8ad81a0930a0b555e37635d8f6897
Justin Lim <Juslwk@gmail.com>
2022-12-17 17:05:24 +0800

    add functions

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,14 @@
-// Import the Firebase SDK for Google Cloud Functions.
+//index.js
+
 const functions = require('firebase-functions');
 const app = require('express')();
-// Import and initialize the Firebase Admin SDK.
-const admin = require('firebase-admin');
-admin.initializeApp();
 
+const {
+    getAllInvitations,
+    getAllGuests
+} = require('./APIs/invitations')
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.get('/invitations', getAllInvitations);
+app.get('/guests', getAllGuests);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+exports.api = functions.https.onRequest(app);
